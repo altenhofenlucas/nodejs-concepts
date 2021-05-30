cc58b6d93c3c89c55b3067dab916c9359c62f7c3
Lucas Altenhofen <lucas.a.bertrand@gmail.com>
2021-05-30 00:13:04 -0300

    Refactoring mais logic, extract to function and added middlewares

--- src/app.js
+++ src/app.js
@@ -10,11 +10,67 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+// function logRequest(req, res, next) {
+//   const { method, url } = req;
+//   const logMessage = `[${method.toUpperCase()}] ${url}`;
+  
+//   console.time(logMessage);
+//   next();
+//   console.timeEnd(logMessage);
+// }
+
+// app.use(logRequest);
+
+function validateRepositoryIdParam(request, response, next) {
+  const { id } = request.params;
+
+  if (!id || !isUuid(id)) {
+    return response.status(400).json({
+      error: 'Check the ID param, is required!'
+    });
+  }
+
+  return next();
+}
+
+function validateRepositoriesEmpty(request, response, next) {
+  if (!repositories || !Array.isArray(repositories) || !repositories.length) {
+    return response.status(400).json({
+      error: 'No repository saved yet! save the first one...'
+    });
+  }
+
+  return next();
+}
+
+function validateRepositoryFound(request, response, next) {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex( repo => repo.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({
+      error: 'Repository not found! check if the ID param is correct and try again...'
+    });
+  }
+
+  request.params.repositoryIndex = repositoryIndex;
+  return next();
+}
+
+app.get("/repositories", validateRepositoriesEmpty, (request, response) => {
   return response.status(200).json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
+app.post("/repositories", createRepository);
+
+app.put("/repositories/:id", validateRepositoryIdParam, validateRepositoryFound, updateRepository);
+
+app.delete("/repositories/:id", validateRepositoryIdParam, validateRepositoryFound, deleteRepository);
+
+app.post("/repositories/:id/like", validateRepositoryIdParam, validateRepositoryFound, likeRepository);
+
+function createRepository(request, response) {
   const { title, url, techs } = request.body;
 
   const newRepository = {
@@ -26,19 +82,13 @@ app.post("/repositories", (request, response) => {
   };
 
   repositories.push(newRepository);
-  
+
   return response.status(201).json(newRepository);
-});
+}
 
-app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params;
+function updateRepository(request, response) {
+  const { id, repositoryIndex } = request.params;
   const {title, url, techs} = request.body;
-
-  const repositoryIndex = repositories.findIndex( repo => repo.id === id);
-
-  if(repositoryIndex < 0) {
-    return response.status(400).json({error: 'Repository not found.'});
-  }
 
   const { likes } = repositories[repositoryIndex];
 
@@ -53,34 +103,22 @@ app.put("/repositories/:id", (request, response) => {
   repositories[repositoryIndex] = updatedRepository;
 
   return response.status(200).json(repositories[repositoryIndex]);
-});
+}
 
-app.delete("/repositories/:id", (request, response) => {
-  const { id } = request.params;
-
-  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
-
-  if (repositoryIndex < 0) {
-    return response.status(400).json({message: 'Repository not found.'});
-  }
+function deleteRepository(request, response) {
+  const { repositoryIndex } = request.params;
 
   repositories.splice(repositoryIndex, 1);
 
   return response.status(204).send();
-});
+}
 
-app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
-
-  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
-
-  if (repositoryIndex < 0) {
-    return response.status(400).json({message: 'Repository not found.'});
-  }
+function likeRepository(request, response) {
+  const { repositoryIndex } = request.params;
 
   repositories[repositoryIndex].likes += 1;
 
   return response.status(200).json(repositories[repositoryIndex]);
-});
+}
 
 module.exports = app;
